be13a6c6f524dc9c8cd77e9e129bb95feaa76c92
Soban Abbas <sobanabbass@gmail.com>
2025-09-09 22:35:52 +0500

    updating readme.md

---
+++ day1/day1.js
@@ -0,0 +1,11 @@
+let a=Symbol();
+
+let user={
+    name:"soban",
+    class:"BSSE",
+    [a]:123
+};
+
+
+console.log(user);
+console.log([a]);
---
+++ day1/reade.md
@@ -0,0 +1,5 @@
+# 1st day 
+- 1 symbol datatype
+- 2 lexical scope 
+- 3 hositing
+- 4 shadownig and illigal shadowning
