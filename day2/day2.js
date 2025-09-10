5f5a24be70e7b4e19505b138b789264faabe12e7
Soban Abbas <sobanabbass@gmail.com>
2025-09-10 21:54:14 +0500

    soving 10 problems

--- day2/day2.js
+++ day2/day2.js
@@ -1,14 +1,41 @@
-let number=document.getElementById("num");
-let checkbtn=document.getElementById("checkbtn");
+// let number =5;
+// do{
+//     console.log(number);
+//     number--;
+// }while(number>=1);
+
+// let array=[2,4,6,7,8];
+// for(let num of array){
+//     console.log(num);
+// }
+// let language="javascript";
+// for(let words of language)
+// {
+//     console.log(words);
+// }
 
 
+// let array=[100,200,300,400,500];
+// function square(value){
+//     console.log(value*value);
+// }
+// array.forEach(square);
 
-checkbtn.addEventListener("click",check);
+// let array=[1,2,3,4,5,6,7,8];
+// for (let item of array){
+//     let result=(item%2===0)?"Even number": "Odd Number";
+//     console.log(item +"is" +result);
+// }
+
+//counting vowals
+let make_string="ceeehhofojpfjfkvkvirihh3prpena xlmwoh";
+ let result=0;
+for(let albt of make_string){
+       
+    if(albt==="a" || albt==="e" || albt === "i" || albt==="o" || albt==="u" ){
+result++;
+    }
 
 
-function check(){
-    let num=Number(number.value);
-    for (let i=1;i<=10;i++){
-    console.log(`${num}*${i}=${num*i}`);
 }
-}
+console.log(result);
