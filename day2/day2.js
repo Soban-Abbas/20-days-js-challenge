13509c9c4b00ca7385bd82a06fe6115905023bf2
Soban Abbas <sobanabbass@gmail.com>
2025-09-10 20:44:38 +0500

    even odd check

--- day2/day2.js
+++ day2/day2.js
@@ -1,29 +1,12 @@
-let array=[
-    {
-    name:"soban",
-    age:20
-},{
-name:"ali",
-age:20,
-}];
-
-for(let items of array){
-    console.log(items);
-}
-//in for each loop 
-// its actually a loop which take a function in it 
-// if we have a array of obj then in fun we pass a value which take the 1 object from array each time and and we can acess obj values like obj.name or obj . age;
-
-let arry2=[1,2,3,4,5,6];
-arry2.forEach(function(value,index){
-    console.log("index : "+index +"value at index" +value);
-})
-arry2.forEach(function(value){
-    console.log(value);
-})
-
-
-array.forEach(function(user){
-    console.log(user.name);
-})
-
+let checkbtn=document.getElementById("checkbtn");
+let input=document.getElementById("num");
+checkbtn.addEventListener("click",check)
+    function check(){
+ if(input.value===""){
+        console.log("Enter Number")
+    }else{
+         let result= (Number(input.value)%2===0)?"Number is even ":"number is odd";
+          console.log(result);
+    }
+  
+    }
