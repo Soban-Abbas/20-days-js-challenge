3d5ed010815c027bbcbf5d78220d350150b43ca8
Soban Abbas <sobanabbass@gmail.com>
2025-09-11 22:42:15 +0500

    understanding reduce

--- day3/day3.js
+++ day3/day3.js
@@ -1,28 +1,29 @@
-let arr=[10,"ali",true,[1,2,3]];
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
-console.log(arr[3][0]);
-// from above and below line  we are getting value at 3 indx which is array then going into the array by writing its index 
-console.log(arr[3][1]);
-console.log(arr[3][2]);
-
-//splice
-let array=[1,2,3,4,5,6,7,8,9];
-
-
-//array.splice(startindex,how many number of item u want to delete from startindex ,add new item at start index and old start index value will move to right side,additem)
-
-array.splice(2,2);//remving from index 2 , 2 item including index 2 and 3
-console.log(array);
-array.splice(2,0,99,100);
-console.log(array);
+// //iterration method
+let fruits=["apple", "banana", "apple", "orange", "banana", "apple"];
+// let box={};
+// for(let i=0;i<fruits.length;i++){
+//    let fruit=fruits[i];
+   
+//    if(box[fruit]){
+//     box[fruit]=box[fruit]+1;
+//    }else{
+//     box[fruit]=1;
+//    }
 
 
-//slice
-//slice extract part of array 
-let colours=['black','white','pink','orange','yellow','red'];
-// Extract elements from index 1 to (but not including) index 5
-let extract=colours.slice(1,5);
-console.log(extract);
+// }
+
+// console.log(box);
+
+let count=fruits.reduce(function(acc,value){
+
+    if(acc[value]){
+        acc[value]=acc[value]+1;
+    }else{
+        acc[value]=1;
+    }
+
+return acc;
+
+},{})
+console.log(count);
