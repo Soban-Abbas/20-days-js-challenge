8f2397dbc79806a46383dbbc76ead513f756f9cc
Soban Abbas <sobanabbass@gmail.com>
2025-09-11 22:48:33 +0500

    adding values into obj in key values pairs dynamically

--- day3/day3.js
+++ day3/day3.js
@@ -1,29 +1,5 @@
-// //iterration method
-let fruits=["apple", "banana", "apple", "orange", "banana", "apple"];
-// let box={};
-// for(let i=0;i<fruits.length;i++){
-//    let fruit=fruits[i];
-   
-//    if(box[fruit]){
-//     box[fruit]=box[fruit]+1;
-//    }else{
-//     box[fruit]=1;
-//    }
-
-
-// }
-
-// console.log(box);
-
-let count=fruits.reduce(function(acc,value){
-
-    if(acc[value]){
-        acc[value]=acc[value]+1;
-    }else{
-        acc[value]=1;
-    }
-
-return acc;
-
-},{})
-console.log(count);
+let box={};
+box["banana"]=1;
+console.log(box);
+box["apple"]=3;
+console.log(box);
