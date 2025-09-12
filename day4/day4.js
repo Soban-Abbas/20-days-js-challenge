1a92eec40852133d6261f5c474684800d1bb4def
Soban Abbas <sobanabbass@gmail.com>
2025-09-12 21:57:11 +0500

    filter pass and failter student

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,13 @@
-let array=[2,4,8,16];
+let students = [
+  { name: "Ali", marks: 40 },
+  { name: "Sara", marks: 70 },
+  { name: "Omar", marks: 55 }
+];
 
-function doubleNumber(n){
-     
-     n.forEach(function(a){
-console.log(a*2)
-     })
-}
-
-
-doubleNumber(array);
+let pass=students.filter(function(e){
+     if(e.marks>=50){
+          console.log(`${e.name} is pass`)
+     }else{
+          console.log(`${e.name} is fail`);
+     }
+})
