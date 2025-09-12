2fbc3fdbdaee61eafa7bc7e16ae9cd72fb27c949
Soban Abbas <sobanabbass@gmail.com>
2025-09-13 00:15:41 +0500

    optional chaining and nullishcoalecing

--- day4/day4.js
+++ day4/day4.js
@@ -1,6 +1,30 @@
-let array=[1,3,5];
-let [a,b=0,c]=array; //default will,be ignored becuase value already exits so b=0 ignore
-console.log(a,b,c);
-console.log(array);
-let [cd="soban",...d]=array;//same 1st value at array[0] exits so "soban " ignored ana 1 will be shown and rest d will collect and show array [3,5];
-console.log(cd,d);
+let student={
+     name:"soban",
+     age:20,
+     address:{
+          city:"wah cantt"
+     }
+}
+
+console.log(student.name);
+//if you try to acess key of main obj and its not exits it will return an undefined so we can assign value by nullish coalecing ??
+//example
+console.log(student.email??"sobanabbass@gmail.com");
+
+
+console.log(student.address.city);
+console.log(student.address.street??9); 
+// here we get an eror because we are accessing key that not present in main obj and in that we are deeply accessing 
+
+// console.log(student.info.class);
+
+// to protect yourdelf from errors
+//try optional chaining so it return undefined
+console.log(student.info?.class);
+
+//after that we can give value to class because now it contain undefinded
+console.log(student.info?.class??"bhatti");
+
+
+
+//********this was about optional chaning and nullish coalecing ***********/
