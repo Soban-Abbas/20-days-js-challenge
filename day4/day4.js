f1e6b00cdfd8adf954f167080486f0327be69dcc
Soban Abbas <sobanabbass@gmail.com>
2025-09-12 20:52:28 +0500

    even odd check with ternary

--- day4/day4.js
+++ day4/day4.js
@@ -1,22 +1,7 @@
-//) Optional Chaining (?.)
-
-//👉 Safe way to access nested properties.
-//If property doesn’t exist, returns undefined instead of error.
-
+function evenodd(n){
+let result=(n%2===0)?"even":"odd";
+console.log(result);
 
 
-let user = {
-     profile: { 
-        name: "Ali" 
-    }
-     };
-
-console.log(user.profile?.name); // "Ali"
-console.log(user.address?.city); // undefined (no error)
-
-
-//Nullish Coalescing (??)
-
-//👉 Provides a default value only if variable is null or undefined (not 0 or "").
-
-console.log(user.address?.city??"wah cantt"); // as here address.city not exist then i give undefined so nullish coalescing porvide value because i give value to varibale which contain undefined or null
+}
+evenodd(52);
