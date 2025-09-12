77b4630b3484b961fb1b34649f1b78b9f0f13096
Soban Abbas <sobanabbass@gmail.com>
2025-09-12 23:46:20 +0500

    destructing and spread and rest  operator

--- day4/day4.js
+++ day4/day4.js
@@ -1,24 +1,6 @@
-let students = [
-  { name: "Ali", marks: 40 },
-  { name: "Sara", marks: 70 },
-  { name: "Omar", marks: 55 },
-  {
-    name: "Alice",
-    totalMarks: 85,
-  },
-];
-
-let highest = students.reduce(function (acc, nxt) {
-  if (acc.length === 0) {
-    acc.push(nxt);
-    return acc;
-  }
-  if (acc[0].marks >= nxt.marks) {
-    return acc;
-  } else {
-    acc.length = 0;
-    acc.push(nxt);
-    return acc;
-  }
-}, []);
-console.log(highest);
+let array=[1,3,5];
+let [a,b=0,c]=array; //default will,be ignored becuase value already exits so b=0 ignore
+console.log(a,b,c);
+console.log(array);
+let [cd="soban",...d]=array;//same 1st value at array[0] exits so "soban " ignored ana 1 will be shown and rest d will collect and show array [3,5];
+console.log(cd,d);
