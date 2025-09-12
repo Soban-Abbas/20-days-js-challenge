e49c525dfa92ec2c2ac3f367483d9a687c2cce06
Soban Abbas <sobanabbass@gmail.com>
2025-09-12 23:27:59 +0500

    finding student with highest marks

--- day4/day4.js
+++ day4/day4.js
@@ -1,13 +1,24 @@
 let students = [
   { name: "Ali", marks: 40 },
   { name: "Sara", marks: 70 },
-  { name: "Omar", marks: 55 }
+  { name: "Omar", marks: 55 },
+  {
+    name: "Alice",
+    totalMarks: 85,
+  },
 ];
 
-let pass=students.filter(function(e){
-     if(e.marks>=50){
-          console.log(`${e.name} is pass`)
-     }else{
-          console.log(`${e.name} is fail`);
-     }
-})
+let highest = students.reduce(function (acc, nxt) {
+  if (acc.length === 0) {
+    acc.push(nxt);
+    return acc;
+  }
+  if (acc[0].marks >= nxt.marks) {
+    return acc;
+  } else {
+    acc.length = 0;
+    acc.push(nxt);
+    return acc;
+  }
+}, []);
+console.log(highest);
