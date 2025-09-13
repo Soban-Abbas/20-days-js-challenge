d5d4f19617a6363c8f1d4f3527fc388acf59972d
Soban Abbas <sobanabbass@gmail.com>
2025-09-13 21:36:16 +0500

    counting btn clicks

--- day5/day5.js
+++ day5/day5.js
@@ -1,22 +1,7 @@
-let email = document.getElementById("email");
-
-let span = document.createElement("span");
-let password = document.getElementById("password");
-let loginform = document.getElementById("loginform");
-let div = document.querySelector("div");
-loginform.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (
-    email.value === "sobanabbass@gmail.com" &&
-    password.value === "soban2669"
-  ) {
-    span.innerText = "";
-    alert("login successfull");
-    loginform.reset();
-  } else {
-    span.innerHTML = "";
-    span.innerText = "Enter valid detail";
-    span.classList.add("red");
-    div.appendChild(span);
-  }
-});
+let btn=document.getElementById("btn");
+let value=1;
+let increase=document.getElementById("increase");
+btn.addEventListener("click",function(e){
+    increase.innerText=`${value++}`;
+    console.log(increase.innerText);
+})
