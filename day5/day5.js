b0ccc860dca750449f8083747624e20bbf3fd311
Soban Abbas <sobanabbass@gmail.com>
2025-09-13 23:27:49 +0500

    live search from array

--- day5/day5.js
+++ day5/day5.js
@@ -1,7 +1,17 @@
-let btn=document.getElementById("btn");
-let value=1;
-let increase=document.getElementById("increase");
-btn.addEventListener("click",function(e){
-    increase.innerText=`${value++}`;
-    console.log(increase.innerText);
-})
+let names = ["Ali", "Sara", "Bilal", "Hina"];
+let input = document.getElementById("search");
+let result = document.getElementById("result");
+
+input.addEventListener("input", function () {
+  let keyword = input.value.toLowerCase();  // user input in lowercase
+
+  // filter array → only items that include the keyword
+  let matches = names.filter(function(name) {
+    //names.filter(...) → go through all names, keep only those that include the keyword.
+    return name.toLowerCase().includes(keyword);
+  });
+console.log(matches);
+  // display result
+
+  result.innerHTML = matches.join("<br>"); //converting array into string seprating by commas or giving line break between items 
+});
