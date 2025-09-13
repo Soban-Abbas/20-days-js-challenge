93ad636d2373732bff68129068fb8851349d5f03
Soban Abbas <sobanabbass@gmail.com>
2025-09-13 21:22:07 +0500

    Login form

--- day5/day5.js
+++ day5/day5.js
@@ -1,6 +1,22 @@
-let input=document.getElementById("input");
-input.addEventListener("keydown",function(e){
-if(e.key==="Enter"){
-    console.log("submitted");
-}
-})
+let email = document.getElementById("email");
+
+let span = document.createElement("span");
+let password = document.getElementById("password");
+let loginform = document.getElementById("loginform");
+let div = document.querySelector("div");
+loginform.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (
+    email.value === "sobanabbass@gmail.com" &&
+    password.value === "soban2669"
+  ) {
+    span.innerText = "";
+    alert("login successfull");
+    loginform.reset();
+  } else {
+    span.innerHTML = "";
+    span.innerText = "Enter valid detail";
+    span.classList.add("red");
+    div.appendChild(span);
+  }
+});
