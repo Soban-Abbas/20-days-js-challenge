7a43e051084c79e26bf18a3372e15ef6d0b85ca5
Soban Abbas <sobanabbass@gmail.com>
2025-09-13 19:57:35 +0500

    mouse eveent

--- day5/day5.js
+++ day5/day5.js
@@ -1,18 +1,11 @@
-document.getElementById("colors").addEventListener("change", function(e) {
-  console.log("You selected:", e.target.value);
-});
+//mouse event
 
-// chnage event is good when we have an option sto choose apply chnage event 
-// when we have an input field when we type and click out of input field then chnage event happen and we can get value but at the same time if we use input event we get live value while typing
-document.getElementById("helo").addEventListener("change",function(e){
-   if (e.target.value === "") { // this will not run becuase chnage evevnt happens only when something chnage in input from previous states 
-    console.log("nothing typed");
-} else {
-    console.log("you typed " + e.target.value);
-}
-
+let box=document.getElementById("box");
+box.addEventListener("mouseover", function(e){
+    box.style.background="green";
+    console.log("mouse Entered");
 })
-
-document.getElementById("helo").addEventListener("input",function(e){
-    console.log("you are typing " + e.target.value)
+box.addEventListener("mouseleave",function(e){
+    box.style.background="lightblue";
+    console.log("mouse out");
 })
