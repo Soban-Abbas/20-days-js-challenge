7ebc4aae82ff3ceb29948629025b679155b351a5
Soban Abbas <sobanabbass@gmail.com>
2025-09-15 23:32:47 +0500

    mouse eveent

--- day7/day.js
+++ day7/day.js
@@ -1,18 +1,13 @@
-let signupform=document.getElementById("signupform");
-let dic2=document.getElementById("div2");
-let userName=document.getElementById("username");
-let password=document.getElementById("password");
-signupform.addEventListener("submit",function(e){
-    e.preventDefault();
-div2.innerText="";
-    if(userName.value.length>3 && password.value.length>6){
-        div.innerText="";
-        alert`form submitted`
-    }else{
-        let createspan=document.createElement("span");
-        createspan.style.color="red";
-        createspan.innerText="Username or Password are not correct"
-        div2.appendChild(createspan);
-    }
+let div=document.querySelector("div");
+div.addEventListener("mouseover" ,function(e){
+console.log("mouse entered")
+    div.style.background="lightgreen";
+    div.style.borderRadius="50%";
 
+})
+div.addEventListener("mouseleave",function(e){
+    div.style.background="lightblue";
+    div.style.width="200px";
+    div.style.height="200px";
+    console.log("mouseleave");
 })
