4471f182aebece0410b55e6489e960f1166f0fce
Soban Abbas <sobanabbass@gmail.com>
2025-09-15 23:10:35 +0500

    applying form validations

--- day7/day.js
+++ day7/day.js
@@ -1,11 +1,18 @@
-let input = document.getElementById("input");
-let ul=document.getElementById("ul");
-input.addEventListener("keydown",function(e){
-    if(e.key==="Enter"){
-   let createli=document.createElement("li");
-   createli.innerText=input.value;
-   ul.appendChild(createli);
-   input.value="";
-
+let signupform=document.getElementById("signupform");
+let dic2=document.getElementById("div2");
+let userName=document.getElementById("username");
+let password=document.getElementById("password");
+signupform.addEventListener("submit",function(e){
+    e.preventDefault();
+div2.innerText="";
+    if(userName.value.length>3 && password.value.length>6){
+        div.innerText="";
+        alert`form submitted`
+    }else{
+        let createspan=document.createElement("span");
+        createspan.style.color="red";
+        createspan.innerText="Username or Password are not correct"
+        div2.appendChild(createspan);
     }
+
 })
