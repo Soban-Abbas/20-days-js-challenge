1de22354f5e4f517317335398f04024a1d4f74f0
Soban Abbas <sobanabbass@gmail.com>
2025-09-18 23:28:47 +0500

    countdown timer

--- Day10/day10.js
+++ Day10/day10.js
@@ -1,28 +1,17 @@
-let theme = document.getElementById("Theme");
-theme.addEventListener("change", function (e) {
-  if (e.target.value === "Themes") {
-    document.body.classList.remove("dark");
-    document.body.classList.remove("light");
-    document.body.classList.add("light");
-  } else if (e.target.value === "dark") {
-    document.body.classList.remove("light");
-    document.body.classList.add("dark");
-    theme.value = "";
-    localStorage.setItem("theme", "dark");
-  } else if (e.target.value === "light") {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
-    localStorage.setItem("theme", "light");
-  }
-});
-window.addEventListener("load", function (a) {
-  let applyTheme = localStorage.getItem("theme");
-  console.log(applyTheme);
-  if (applyTheme !== null && applyTheme === "dark") {
-    document.body.classList.add("dark");
-    theme.value = "Dark";
-  } else if (applyTheme !== null && applyTheme === "light") {
-    document.body.classList.add("light");
-    theme.value = "Light";
-  }
-});
+let inputTime=document.getElementById("inputtime");
+let btn=document.getElementById("btn");
+let span=document.getElementById("span");
+btn.addEventListener("click",function(e){
+    console.log(Number(inputTime.value));
+span.innerText=Number(inputTime.value);
+let timer=Number(inputTime.value);
+let stop=setInterval(() => {
+    timer--;
+    span.innerText=timer;
+    if(timer===0){
+        alert("Time Up")
+clearInterval(stop);
+    }
+}, 1000);
+
+})
