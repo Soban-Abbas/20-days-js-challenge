6f72b3b65767f9b60afea23c39dde591c3637469
Soban Abbas <sobanabbass@gmail.com>
2025-09-20 22:27:14 +0500

    weather app by catch then finally

--- day12/day12_weatherapi.js
+++ day12/day12_weatherapi.js
@@ -1,32 +1,42 @@
-let locationInput=document.getElementById("location");
-let searchBtn=document.getElementById("searchbtn");
-searchBtn.disabled=true;
-locationInput.addEventListener("input",function(){
-    if(locationInput.value===""){
-        searchBtn.disabled=true;
-    }else{
-        searchBtn.disabled=false;
+let locationInput = document.getElementById("location");
+let show = document.getElementById("div");
+let searchBtn = document.getElementById("searchbtn");
+searchBtn.disabled = true;
+locationInput.addEventListener("input", function () {
+  if (locationInput.value === "") {
+    searchBtn.disabled = true;
+  } else {
+    searchBtn.disabled = false;
+  }
+});
 
-    }
+searchBtn.addEventListener("click",  function (e) {
+  let city = locationInput.value.trim();
+  console.log(locationInput.value);
+
+  const key = `http://api.weatherapi.com/v1/current.json?key=57f2f7300faa435eaee151111252009&q=${city}`;
+
+   function weatherapi() {
+    let result =  fetch(key)
+    .then(function(response){
+return response.json();
+
+    })
+    .then(function(data){
+ show.innerHTML = `Temperatur in ${city} is ${data.current.temp_c} C <br> Wind : ${data.current.wind_kph}km/h <br> Humidity : ${data.current.humidity}% <br> Condition : ${data.current.condition.text}`;
+    })
+    .catch(function(error){
+        show.innerHTML=error;
+    }).finally(function(){
+        console.log('Process Done');
+    })
     
-})
+  }
 
+//   let data = await weatherapi();
 
-searchBtn.addEventListener("click", async function(e){
-    let city=locationInput.value.trim();
-    console.log(locationInput.value );
-
-    const key= `http://api.weatherapi.com/v1/current.json?key=57f2f7300faa435eaee151111252009&q=${city}`;
-
-    async function weatherapi() {
-        let result=await fetch(key)
-        return result.json();
-    }
-
-let data= await weatherapi();
-
-let show=document.getElementById("div");
-show.textContent=`Temperatur in ${city} is ${data.current.temp_c} C`;
-
-})
-
+//   
+//  
+weatherapi();
+  locationInput.value = "";
+});
