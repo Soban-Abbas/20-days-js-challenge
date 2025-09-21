21288655dcdabf28e115cb7cff676f1c6e84b2f6
Soban Abbas <sobanabbass@gmail.com>
2025-09-21 14:26:10 +0500

    weather app without await in async  function call

---
+++ day13/day13.js
@@ -0,0 +1,33 @@
+let locationInput=document.getElementById("location");
+let searchBtn=document.getElementById("searchbtn");
+searchBtn.disabled=true;
+locationInput.addEventListener("input",function(){
+    if(locationInput.value===""){
+        searchBtn.disabled=true;
+    }else{
+        searchBtn.disabled=false;
+
+    }
+    
+})
+
+searchBtn.addEventListener("click",  function(e){
+    let city=locationInput.value.trim();
+    console.log(locationInput.value );
+
+    const key= `http://api.weatherapi.com/v1/current.json?key=57f2f7300faa435eaee151111252009&q=${city}`;
+
+    async function weatherapi() {
+        let result=await fetch(key)
+        let data= await
+         result.json();
+
+        
+let show=document.getElementById("div");
+show.textContent=`Temperatur in ${city} is ${data.current.temp_c} C`;
+    }
+
+weatherapi();
+
+
+})
