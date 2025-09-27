9e04acda5af21eb6395c589feb1cf85014d18ade
Soban Abbas <sobanabbass@gmail.com>
2025-09-27 14:08:59 +0500

    just checking nav abr

--- day19/day19.js
+++
@@ -1,6 +0,0 @@
-let icon = document.querySelector(".icon");
-let ul = document.querySelector("ul");
-icon.addEventListener("click", function (e) {
-  ul.classList.toggle("showdata");
-  console.log(ul);
-});
